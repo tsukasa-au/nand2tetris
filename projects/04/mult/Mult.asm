bc9ba8e996f956852ee65e101203e5dd296ce827
Greg Darke <greg@tsukasa.net.au>
2021-09-07 21:36:41 +1000

    Variation on the addition based multiply (8x faster)

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -18,6 +18,46 @@ M=0
 // Load R1 into a temporary (this is our counter that we will decrement)
 @R1
 D=M
+
+// Handle the case where R1 is not divisible by 8
+@8
+D=A-D
+@7
+D=D&A
+@PRE_LOOP
+D;JLE  // If R0 % 8 == 0; jump PRE_LOOP
+A=D
+D=D+A
+@MUL_UNROLL
+A=A+D
+D=0;JMP
+(MUL_UNROLL)
+@R0
+D=D+M
+@R0
+D=D+M
+@R0
+D=D+M
+@R0
+D=D+M
+@R0
+D=D+M
+@R0
+D=D+M
+@R0
+D=D+M
+@R0
+D=D+M
+
+@R2
+M=D
+
+(PRE_LOOP)
+@R1
+D=M
+@7
+A=!A
+D=D&A
 @mul_remaining
 M=D
 @DONE
@@ -27,11 +67,16 @@ D;JLE // if mul_remaining <= 0; jump DONE // No more iterations required
 
 // Perform one step: (R2 += R0)
 @R0
-D=M
+AD=M    // D = R0
+AD=D+A  // D = 2*R0
+AD=D+A  // D = 4*R0
+AD=D+A  // D = 8*R0
 @R2
-M=D+M
+M=D+M  // R2 += 8*R0
+@8
+D=A
 @mul_remaining
-MD=M-1
+MD=M-D  // mul_remaining -= 4
 @LOOP
 D;JGT  // If mul_remaining > 0; jump LOOP
 
